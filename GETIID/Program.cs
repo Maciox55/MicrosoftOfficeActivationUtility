e6fef5ffb883246d5b49d76ffd6a3e55cc48e604
uduser <uduser@UNHOLTZ-PC>
2021-02-19 16:28:48 -0500

    Added PortableSettingsProvider

--- GETIID/Program.cs
+++ GETIID/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Bluegrams.Application;
 namespace GETIID
 {
     static class Program
@@ -15,6 +15,9 @@ namespace GETIID
         static void Main()
         {
             Application.EnableVisualStyles();
+            PortableSettingsProvider.SettingsFileName = "settings.config";
+            PortableSettingsProvider.ApplyProvider(Properties.Settings.Default);
+
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
